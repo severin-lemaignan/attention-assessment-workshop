0c35ddee396e01d4fa7701760fa635db643c3583
Séverin Lemaignan <severin@guakamole.org>
2015-08-20 22:47:24 +0100

    Changed code layout a little bit

--- templates/intro/src/head_detection.cpp
+++ templates/intro/src/head_detection.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 {
     if(argc < 2) {
         cerr << "Usage: " << endl <<
-                "head_detection model.dat (eg: head_detection ../../share/shape_predictor_68_face_landmarks.dat)" << endl;
+                "head_detection model.dat (eg: head_detection ../share/shape_predictor_68_face_landmarks.dat)" << endl;
         return 1;
     }
 
